3714967ba1f8735b714e1a0f7cb3150bd8439c79
Saw Kyaw Walar Htwe <107252451+sawkyawwalarhtwe@users.noreply.github.com>
2023-10-31 01:00:18 +0630

    otherfix

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/components/footer";
+import NavBar from "@/components/navigatoin";
+
+export default function HomePage() {
+  return (
+    <main className=' w-full overflow-hidden'>
+     <NavBar/>
+     <Footer/>
+    </main>
+  )
+}
