a5329851b90705188f0efa8529b3ab74b53c2b0b
Saw Kyaw Walar Htwe <107252451+sawkyawwalarhtwe@users.noreply.github.com>
2023-10-31 03:22:16 +0630

    add the rest section of the main page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,6 +3,12 @@ import Footer from "@/components/footer";
 import NavBar from "@/components/navigatoin";
 import MAbout from "./about";
 import SlideShow from "./slideshow";
+import MVideos from "./video";
+import MainNews from "./news";
+import MEvents from "./events";
+import Join from "./join";
+import MupcomingTraining from "./upcomingTraining";
+import MPublication from "./publication";
 export default function HomePage() {
   return (
     <main className=' w-full overflow-hidden'>
@@ -10,6 +16,16 @@ export default function HomePage() {
      <SlideShow/>
       <hr className='mx-[6%]'/> 
       <MAbout/>
+      <MVideos/>
+      <hr className='mx-[6%]'/>
+      <MainNews/>
+      <MEvents/>
+      <hr className='mx-[6%]'/>
+      <Join/>
+      <hr className='mx-[6%]'/>
+      <MupcomingTraining/>
+      <hr className='mx-[6%]'/>
+      <MPublication/>
      <Footer/>
     </main>
   )
